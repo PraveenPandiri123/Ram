d54c902cb95eb8657c74cc733ba625115a847db1
praveenpandiri <praveenpandiri171@gmail.com>
2024-12-26 22:12:07 +0530

    print praveen branch

--- src/com/ram/Test.java
+++ src/com/ram/Test.java
@@ -4,10 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("praveen is good boy");
-System.out.println("dev");
-System.out.println("dev2git add."
-		+ "");
+		System.out.println("praveen is good boy");
+		System.out.println("dev");
+		System.out.println("dev2git add." + "");
+		System.out.println("Wellcome to clarity world");
 	}
 
 }
